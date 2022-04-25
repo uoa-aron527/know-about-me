72b28e197df020c56ee5f65967387c72b5c823b3
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-04-26 11:21:42 +1200

    Initiated Navbar design component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-cyan-200 text-2xl'> Let's make this </h1>
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[60px] flex justify-between items-center bg-indigo-700 px-4 text-gray-200'>
+        {/* Insert Logo here from the /assets directory */}
+    </div>
+  )
+}
+
+export default Navbar;
