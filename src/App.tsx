63454dfd19ec573f09e1c4fbaf8d889ea0a97e14
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-04-26 10:51:16 +1200

    Setup for Typescript and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-cyan-200 text-2xl'> Let's make this </h1>
+    </div>
+  );
+}
+
+export default App;
