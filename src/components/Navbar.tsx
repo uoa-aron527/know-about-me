cdb63ba263bd96b274c8706406208221d997ff6a
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-04-27 23:38:31 +1200

    Navbar updated with components and transitions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {GiHamburgerMenu} from 'react-icons/gi';
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[60px] flex justify-between items-center bg-indigo-700 px-4 text-gray-200'>
+    <div className='fixed w-full h-[60px] flex justify-center items-center bg-indigo-700 px-4 text-gray-200'>
         {/* Insert Logo here from the /assets directory */}
+        {/* Menubar for contents */}
+        <div>
+          <ul className='flex'>
+            <li> Home </li>
+            <li> About </li>
+            <li> Skills </li>
+            <li> Projects </li>
+            <li> Contact </li>
+          </ul>
+        </div>
+        <div className='hidden'>
+          <GiHamburgerMenu/>
+        </div>
     </div>
   )
 }
