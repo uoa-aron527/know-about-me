7e3f86fea1ec5144c92de3e708ada507b7bbceb4
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-04-28 15:06:48 +1200

    Added responsive design template for mobile screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,19 +7,29 @@ const Navbar: React.FC<NavbarProps> = () => {
   return (
     <div className='fixed w-full h-[60px] flex justify-center items-center bg-indigo-700 px-4 text-gray-200'>
         {/* Insert Logo here from the /assets directory */}
-        {/* Menubar for contents */}
-        <div>
-          <ul className='flex'>
-            <li> Home </li>
-            <li> About </li>
-            <li> Skills </li>
-            <li> Projects </li>
-            <li> Contact </li>
-          </ul>
-        </div>
-        <div className='hidden'>
+        {/* Menubar for contents in full large screen view*/}
+        <ul className='hidden md:flex'>
+          <li> Home </li>
+          <li> About </li>
+          <li> Skills </li>
+          <li> Projects </li>
+          <li> Contact </li>
+        </ul>
+        <div className='md:hidden w-full z-10 flex justify-between items-center'>
+          <div>Logo</div>
           <GiHamburgerMenu/>
         </div>
+
+        {/* Smaller screen view (mobile) */}
+
+        <ul className='md:hidden absolute top-0 left-0 w-full h-screen bg-indigo-700 px-4 text-gray-200 flex flex-col justify-center items-center'>
+          <li className='py-[14px] text-4xl mb-2'> Home </li>
+          <li className='py-[14px] text-4xl mb-2'> About </li>
+          <li className='py-[14px] text-4xl mb-2'> Skills </li>
+          <li className='py-[14px] text-4xl mb-2'> Projects </li>
+          <li className='py-[14px] text-4xl mb-2'> Contact </li>
+        </ul>
+
     </div>
   )
 }
