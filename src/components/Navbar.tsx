76dded4aabfb49b3bddb5e6b2a31cb964ddda230
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-05-07 18:37:10 +1200

    Changed colour palette of navbar with new hover effects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <div className='fixed w-full h-[60px] flex justify-center items-center bg-indigo-700 px-4 text-gray-200'>
+    <div className='fixed w-full h-[60px] flex justify-center items-center bg-gray-900 px-4 text-gray-200'>
         {/* Insert Logo here from the /assets directory */}
         {/* Menubar for contents in full large screen view*/}
         <ul className='hidden md:flex'>
@@ -22,7 +22,7 @@ const Navbar: React.FC<NavbarProps> = () => {
 
         {/* Smaller screen view (mobile) */}
 
-        <ul className='md:hidden absolute top-0 left-0 w-full h-screen bg-indigo-700 px-4 text-gray-200 flex flex-col justify-center items-center'>
+        <ul className='md:hidden absolute top-0 left-0 w-full h-screen bg-gray-900 px-4 text-gray-200 flex flex-col justify-center items-center'>
           <li className='py-[14px] text-4xl mb-2'> Home </li>
           <li className='py-[14px] text-4xl mb-2'> About </li>
           <li className='py-[14px] text-4xl mb-2'> Skills </li>
