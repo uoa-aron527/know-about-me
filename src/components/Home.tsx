ee7925f01fd489e1bd474d20db742f32d51567b3
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-06-10 23:19:26 +1200

    Inserted first few text as p tags

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,8 +5,8 @@ interface HomeProps{}
 const Home: React.FC<HomeProps> = () => {
   return (
     <div className='bg-gray-800 h-screen w-full'>
-      <div className='max-w-5xl mx-auto px-7 flex flex-col justify-center h-full'>
-        {/* <p className='text-red-500'> I am Aryan Sharma Ronee </p> */}
+      <div className='max-w-5xl mx-auto flex flex-col justify-center h-full'>
+        <p className='text-red-500 text-center text-2xl'> I am Aryan Sharma Ronee </p>
       </div>
     </div>
   )
